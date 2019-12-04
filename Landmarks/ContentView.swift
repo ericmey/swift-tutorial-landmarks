445cb42c68c3c1a55c19029abc89ea5325c4adde
Eric Mey <ericmey@gmail.com>
2019-12-04 00:10:50 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Eric Mey on 12/4/19.
+//  Copyright © 2019 Eric Mey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

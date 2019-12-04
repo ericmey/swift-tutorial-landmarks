95dadf8728bbb8d2b0c6c7150544ef2b20a72952
Eric Mey <ericmey@gmail.com>
2019-12-04 02:07:22 -0500

    Initial Commit

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Eric Mey on 12/4/19.
+//  Copyright © 2019 Eric Mey. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(x: 0, y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack(alignment: .top) {
+                    Text(landmark.park)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+
+            Spacer()
+        }
+        .navigationBarTitle(Text(verbatim: landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,46 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Eric Mey on 12/4/19.
+//  Copyright © 2019 Eric Mey. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    
+    var state: String
+    var park: String
+    var category: Category
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
